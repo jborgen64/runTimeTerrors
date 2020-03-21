8b275b5a386b4bf3aa9cbb34bbca7d977959fb28
tboyd4 <tboyd1898@gmail.com>
2020-03-21 09:20:58 -0500

    controller now displaying index (signup) and the login handlebars files

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -2,15 +2,26 @@ var express = require("express");
 
 var router = express.Router();
 
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 // Create routes
 router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burger: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+    
+    res.render("index");
+  
+});
+
+router.get("/login", function(req, res) {
+  // If the user already has an account send them to the members page
+  if (req.user) {
+    res.redirect("/members");
+  }
+  res.render("login");
+});
+
+router.get("/members", isAuthenticated, function(req, res) {
+  res.render("testdash");
 });
 
 
