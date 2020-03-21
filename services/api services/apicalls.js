e56582236f3e7d2a6fd63a70561444ed78f097c8
clarkdkyle <clarkdkyle@gmail.com>
2020-03-21 13:33:18 -0500

    searching4me

--- services/api services/apicalls.js
+++ services/api services/apicalls.js
@@ -1,15 +1,19 @@
-$('#search').on('submit',function() {
+
+$('#button').on('click',function() {
 
 searchparam = $("#search").val()
 
-    axios.get(`http://localhost:3000/https://comicvine.gamespot.com/api/characters?api_key=6d585bd220603de589bc80707c5dbd370ac7f030&format=json&sort=name:${searchparam}&field_list=name`)
-        .then(function (response) {
+    $.ajax({
+        method: 'GET',
+        url: `https://comicvine.gamespot.com/api/characters?api_key=6d585bd220603de589bc80707c5dbd370ac7f030&format=json&filter=name:${searchparam}&field_list=name`
+    }) .then(function (response) {
             console.log(response);
         })
-        .catch(function (error) {
-            console.log(error);
-        })
+      
 });
+
+    // axios.get(`http://localhost:3000/https://comicvine.gamespot.com/api/characters?api_key=6d585bd220603de589bc80707c5dbd370ac7f030&format=json&sort=name:${searchparam}&field_list=name`)
+       
 
 // var issuesearch =
 //     axios.get('http://localhost:3000/https://comicvine.gamespot.com/api/issues?api_key=6d585bd220603de589bc80707c5dbd370ac7f030&format=json&sort=cover_date:desc')
