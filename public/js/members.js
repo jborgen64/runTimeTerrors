9ccb7d68dd832956e69848b8a21686c8bd01ed30
son loku <jacobrborgen@gmail.com>
2020-03-24 00:08:44 -0500

    added functionality to search bar

--- public/js/members.js
+++ public/js/members.js
@@ -1,10 +1,23 @@
-//nav items display dashboard and seach functions
-$('#searchDisplay').on("click", function(){
+//nav items display dashboard and seach divs
+$('#searchDisplay').on('click', function(){
     $('.dashboard').hide(200);
     $('.searchContainer').show(200);
 });
 
-$('#dashDisplay').on("click", function(){
+$('#dashDisplay').on('click', function(){
     $('.searchContainer').hide(200);
     $('.dashboard').show(200);
+});
+
+//when clicked search button will make API call
+$('.searchBtn').on('click', function(){
+
+    //API call here - set returned value to results
+// <==================>
+    const results = $('.searchItem').val().trim();
+    console.log(results);
+    const resultsDisp = $(`<div class="searchInput">${results}<div>`);
+    $('.searchResult').append(resultsDisp);
+
+    
 });
