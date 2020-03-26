4a28a2b0958fa743e78ab0661454419c6529bfac
tboyd4 <tboyd1898@gmail.com>
2020-03-25 19:26:13 -0500

    save button testing. api put call is working, getting unauthorized error 401

--- public/js/members.js
+++ public/js/members.js
@@ -1,27 +1,64 @@
-
-
 //nav items display dashboard and seach divs
-$('#searchDisplay').on('click', function(){
-    $('.dashboard').hide(200);
-    $('.searchContainer').show(200);
+$("#searchDisplay").on("click", function() {
+  $(".dashboard").hide(200);
+  $(".searchContainer").show(200);
 });
 
-$('#dashDisplay').on('click', function(){
-    $('.searchContainer').hide(200);
-    $('.dashboard').show(200);
+$("#dashDisplay").on("click", function() {
+  $(".searchContainer").hide(200);
+  $(".dashboard").show(200);
 });
 
 //when clicked search button will make API call
-$('.searchBtn').on('click', function(){
-
-    //API call here - set returned value to results
-// <==================>
-    const results = $('.searchItem').val().trim();
-    console.log(results);
-    const resultsDisp = $(`<div class="searchInput">${results}<div>`);
-    $('.searchResult').append(resultsDisp);
+$(".searchBtn").on("click", function() {
+  //API call here - set returned value to results
+  // <==================>
+  const results = $(".searchItem")
+    .val()
+    .trim();
+  console.log(results);
+  const resultsDisp = $(`<div class="searchInput">${results}<div>`);
+  $(".searchResult").append(resultsDisp);
 });
 
+//================================================================//
+//================================================================//
+
+// code below is for saving data to database, and receiving data from database //
+
+jQuery.each(["put", "delete"], function(i, method) {
+  jQuery[method] = function(url, data, callback, type) {
+    if (jQuery.isFunction(data)) {
+      type = type || callback;
+      callback = data;
+      data = undefined;
+    }
+
+    return jQuery.ajax({
+      url: url,
+      type: method,
+      dataType: type,
+      data: data,
+      success: callback
+    });
+  };
+});
+
+$("#savebutton").on("click", function() {
+  $.put("/api/save/:id", {
+    title: "newsave",
+    title1: "anothernewsave"
+  })
+    .then(function() {
+      console.log("I tried to put");
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+});
+
+//================================================================//
+//================================================================//
 
 // //rotating background for each click
 // const backgroundPics = ['superman.jpeg', 'wolverine.jpeg', 'wonder.jpeg', 'martian.jpeg'];
@@ -29,4 +66,3 @@ $('.searchBtn').on('click', function(){
 // backgroundPics.forEach(function(pics) {
 //     $('.background').css({'background-image': `url(/images/${pics[i]})`);
 // });
-    
