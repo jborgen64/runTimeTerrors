9b9910ebd5b6c1228e28f05965f8a5402fb22ea7
son loku <jacobrborgen@gmail.com>
2020-03-28 08:03:59 -0500

    added responsive functionality and some styling touchups to page

--- public/js/members.js
+++ public/js/members.js
@@ -44,7 +44,8 @@ $(".searchBtn").on("click", function() {
       var issue = {
         title: res.results[i].name,
         cover: res.results[i].image.medium_url,
-        issuenum: res.results[i].issue_number
+        issuenum: res.results[i].issue_number,
+        issueId: res.results[i].id
       };
 
       //pushing issues into empty array
@@ -141,6 +142,8 @@ const saveNewComic = (title, issueNum, urlPic) => {
       newObj.urlPic = urlPic;
       newObj.issueNum = issueNum;
 
+      let savingArray = [];
+
       response.savingArray.push(newObj);
 
       $.put("/api/save/" + currentUser, response)
@@ -170,7 +173,7 @@ $("#savedTitles").on("click", function() {
       console.log("I tried to get");
       console.log(response);
 
-      let savedArray = response.savingArray;
+      var savedArray = response.savingArray;
       let issueArr = [];
 
       for (var i = 0; i < savedArray.length; i++) {
